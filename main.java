bdb9acdc79f95c5083ca46d86e7625c2e1413c71
AletaDayinta <129129383+AletaDayinta@users.noreply.github.com>
2023-03-28 08:19:20 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alatmusik;
+
+public class main extends AlatMusik {
+    public static void main(String[] args) {
+        AlatMusik alatmusikkk =new AlatMusik();
+        piano pianooo =new piano();
+        biola biolaaa =new biola();
+        gitar gitarrr =new gitar();
+        drum drummm =new drum();
+        saxophone saxophoneee =new saxophone();
+        trumpet trumpettt =new trumpet();
+        
+        alatmusikkk.njajal();
+        pianooo.njajal();
+        biolaaa.njajal();
+        gitarrr.njajal();
+        drummm.njajal();
+        saxophoneee.njajal();
+        trumpettt.njajal();
+    }
+}
